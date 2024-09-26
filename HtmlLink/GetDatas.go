604dbe1eee09b447c89a137a5dc70707c5f49be6
gus-du-94 <ludovic.dumet1@gmail.com>
2024-09-26 15:12:18 +0200

    StartUpdate

--- HtmlLink/GetDatas.go
+++ HtmlLink/GetDatas.go
@@ -12,11 +12,11 @@ import (
 
 func HandleAdminPage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("HandleAdminPage")
-	datas := GetDatas()
-	data := map[string]interface{}{
-		"Projets": datas,
-	}
 	if r.Method == http.MethodGet {
+		datas := GetDatas()
+		data := map[string]interface{}{
+			"Projets": datas,
+		}
 		idToGet := r.FormValue("id")
 		if idToGet != "" {
 			id, err := strconv.Atoi(idToGet)
@@ -38,6 +38,16 @@ func HandleAdminPage(w http.ResponseWriter, r *http.Request) {
 		Span := r.FormValue("Durée")
 		PostData(nom, description, DateDebut, DateFin, Span)
 	}
+	if r.Method == http.MethodPut {
+		log.Print("test")
+		id := r.FormValue("id")
+		nom := r.FormValue("updatePfName")
+		description := r.FormValue("updatePfDescription")
+		DateDebut := r.FormValue("updateStartDate")
+		DateFin := r.FormValue("updateEndDate")
+		Span := r.FormValue("updateDuration")
+		UpdateData(id, nom, description, DateDebut, DateFin, Span)
+	}
 }
 
 func PostData(nom string, description string, DateStart string, DateFin string, Span string) {
@@ -46,6 +56,19 @@ func PostData(nom string, description string, DateStart string, DateFin string, 
 		log.Fatal(err)
 	}
 	GestionBDD.PostProjet(db, nom, description, DateStart, DateFin, Span)
+}
+
+func UpdateData(id string, nom string, description string, DateStart string, DateFin string, span string) {
+	db, err := sql.Open("sqlite3", "database/database.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	idInt, _ := strconv.Atoi(id)
+	GestionBDD.UpdateDateDebutById(db, idInt, DateStart)
+	GestionBDD.UpdateDateFinById(db, idInt, DateFin)
+	GestionBDD.UpdateDescriptionById(db, idInt, description)
+	GestionBDD.UpdateProjetNameById(db, idInt, nom)
+	GestionBDD.UpdateSpanById(db, idInt, span)
 }
 
 func HandleIndexPage(w http.ResponseWriter, r *http.Request) {
