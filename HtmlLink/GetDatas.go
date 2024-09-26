1c70e5a25686a45082ec7e6a541287238fab5eca
Gabypic <gpicard77350@gmail.com>
2024-09-26 10:42:13 +0200

    getbyid

--- HtmlLink/GetDatas.go
+++ HtmlLink/GetDatas.go
@@ -7,27 +7,49 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func HandleAdminPage(w http.ResponseWriter, r *http.Request) {
-	datas := GetDatas(w, r)
+	fmt.Println("HandleAdminPage")
+	datas := GetDatas()
 	data := map[string]interface{}{
 		"Projets": datas,
 	}
-	fmt.Println("HandleAdminPage", datas)
 	if r.Method == http.MethodGet {
+		idToGet := r.FormValue("id")
+		if idToGet != "" {
+			id, err := strconv.Atoi(idToGet)
+			if err == nil {
+				dataByIdTemp := GetDatasById(id)
+				data["SelectedProject"] = dataByIdTemp
+			} else {
+				log.Println("Invalid ID:", err)
+			}
+		}
 		templates.RenderTemplate(w, "admin", data)
 		return
 	}
 }
 
-func GetDatas(w http.ResponseWriter, r *http.Request) []GestionBDD.Projet {
-
+func GetDatas() []GestionBDD.Projet {
 	db, err := sql.Open("sqlite3", "database/database.db")
 	if err != nil {
 		log.Fatal(err)
 	}
 	datas, err := GestionBDD.GetAllProjet(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return datas
+}
+
+func GetDatasById(id int) GestionBDD.Projet {
+	db, err := sql.Open("sqlite3", "database/database.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	datas, err := GestionBDD.GetProjetById(db, id)
 	if err != nil {
 		log.Fatal(err)
 	}
