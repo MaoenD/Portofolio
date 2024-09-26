59c82319f2d815962e8262ade7a00d80da4b7625
Gabypic <gpicard77350@gmail.com>
2024-09-26 12:42:36 +0200

    server push to pull

--- server/server.go
+++ server/server.go
@@ -22,7 +22,7 @@ func Start() {
 	http.Handle("/Style/", http.StripPrefix("/Style/", http.FileServer(http.Dir("Style"))))
 	http.Handle("/ressource/", http.StripPrefix("/ressource/", http.FileServer(http.Dir("ressource"))))
 
-	GestionBDD.PostProjet(db, "test", "test", "test", "test", "test")
+	GestionBDD.PostProjet(db, "El Project", "Un magnifique projet", "08/2024", "09/2024", "1mois")
 
 	http.HandleFunc("/admin", HtmlLink.HandleAdminPage)
 	http.HandleFunc("/index", HtmlLink.HandleIndexPage)
