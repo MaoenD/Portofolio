babf4d69d187e7b0c8f20154b1cf6c50413e3982
Gabypic <gpicard77350@gmail.com>
2024-09-27 09:22:13 +0200

    rename HTMLlinks files

--- server/server.go
+++ server/server.go
@@ -1,11 +1,8 @@
 package server
 
 import (
-	"Portefolio/GestionBDD"
 	"Portefolio/HtmlLink"
 	"Portefolio/database"
-	"database/sql"
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -15,16 +12,9 @@ func Start() {
 	cfg := database.ReadDB()
 	database.ConnectDB(cfg.DatabaseURL)
 	database.CreateTable()
-	db, err := sql.Open("sqlite3", "database/database.db")
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	http.Handle("/Style/", http.StripPrefix("/Style/", http.FileServer(http.Dir("Style"))))
 	http.Handle("/ressource/", http.StripPrefix("/ressource/", http.FileServer(http.Dir("ressource"))))
-	fmt.Println("caca")
-
-	GestionBDD.PostProjet(db, "El Project", "Un magnifique projet", "08/2024", "09/2024", "1mois")
 
 	http.HandleFunc("/admin", HtmlLink.HandleAdminPage)
 	http.HandleFunc("/index", HtmlLink.HandleIndexPage)
